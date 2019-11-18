75152c7aefaeb95ebea169e675c75f4c9a2ffd69
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2019-11-18 11:10:38 +0500

    Update Program.cs

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -4,7 +4,7 @@ namespace TestProject
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Mai(string[] args)
         {
             Console.WriteLine("Hello, world");
             Console.WriteLine("{0}+{1}={2}",1.1,1.2,(1.1+1.2));
