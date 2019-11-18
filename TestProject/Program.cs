2023bfa4e77f6994d0509645164569802c8949c5
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2019-11-18 15:05:02 +0500

    Style fix

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -7,7 +7,7 @@ namespace TestProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, world");
-            Console.WriteLine("{0}+{1}={2}",1.1,1.2,(1.1+1.2));
+            Console.WriteLine("{0}+{1}={2}", 1.1, 1.2, (1.1+1.2));
             
             try{ Console.ReadKey(); }
             catch(Exception ex){ Console.WriteLine(ex.GetType().ToString()+": "+ex.Message); }
