a6268f3f128c314e1f0c52a1f773a4efc3f99de2
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2019-11-18 11:44:46 +0500

    Update Program.cs

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -6,7 +6,7 @@ namespace TestProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world");
+            Console.WriteLine("Hello, world")
             Console.WriteLine("{0}+{1}={2}",1.1,1.2,(1.1+1.2));
             try{ Console.ReadKey(); }
             catch(Exception ex){ Console.WriteLine(ex.ToString()+": "+ex.Message); }
